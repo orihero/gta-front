468827c5be1c5a4c96e838ed2c381505c4db065d
OriHero <aka.orihero@gmail.com>
2021-02-21 23:26:12 +0500

    Change

--- src/api/requests.js
+++ src/api/requests.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export let url = 'http://localhost:8090';
+// export let url = 'http://localhost:8090';
 export let url = 'http://134.0.116.109:8090';
 export let apiUrl = `${url}/api`;
 
